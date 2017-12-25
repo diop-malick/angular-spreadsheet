e2fa25781cd0e7861aad3db94bb9a327500f4415
Malick DIOP <diop-malick@hotmail.fr>
2017-12-25 17:43:02 +0000

    Material > Data Table with sort, filter and pagination

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
 
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
+import { MatPaginator, MatSort } from '@angular/material';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import '../rxjs-operators';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
+  // heroes: Array<any>;
+
+  displayedColumns = ['id', 'name', 'phone'];
+  exampleDatabase = new HeroDatabase();
+  dataSource: HeroDataSource | null;
+
+  @ViewChild('filter') filter: ElementRef;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
+    this.dataSource = new HeroDataSource(
+      this.exampleDatabase,
+      this.sort,
+      this.paginator
+    );
+    Observable.fromEvent(this.filter.nativeElement, 'keyup')
+      .debounceTime(150)
+      .distinctUntilChanged()
+      .subscribe(() => {
+        if (!this.dataSource) {
+          return;
+        }
+        this.dataSource.filter = this.filter.nativeElement.value;
+      });
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+    this.heroService.getHeroes().subscribe(heroes => {
+      this.heroes = heroes;
+      dataChange.next(heroes);
+    });
   }
 
   add(name: string): void {
@@ -35,4 +68,103 @@ export class HeroesComponent implements OnInit {
     this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
   }
+}
+
+/** Stream that emits whenever the data has been modified. */
+
+const dataChange: BehaviorSubject<Hero[]> = new BehaviorSubject<Hero[]>([]);
+
+/** An example database that the data source uses to retrieve data for the table. */
+
+export class HeroDatabase {
+  get data(): Hero[] {
+    return dataChange.value;
+  }
+
+  constructor() {}
+}
+
+/**
+ * Data source to provide what data should be rendered in the table. Note that the data source
+ * can retrieve its data in any way. In this case, the data source is provided a reference
+ * to a common data base, IssueData. It is not the data source's responsibility to manage
+ * the underlying data. Instead, it only needs to take the data and send the table exactly what
+ * should be rendered.
+ */
+export class HeroDataSource extends DataSource<any> {
+  _filterChange = new BehaviorSubject('');
+  get filter(): string {
+    return this._filterChange.value;
+  }
+  set filter(filter: string) {
+    this._filterChange.next(filter);
+  }
+
+  filteredData: any[] = [];
+  renderedData: any[] = [];
+
+  constructor(
+    private _exampleDatabase: HeroDatabase,
+    private _sort: MatSort,
+    private _paginator: MatPaginator
+  ) {
+    super();
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<any[]> {
+    const displayDataChanges = [
+      dataChange,
+      this._filterChange,
+      this._sort.sortChange,
+      this._paginator.page
+    ];
+
+    return Observable.merge(...displayDataChanges).map(() => {
+      // Filter data
+      this.filteredData = this._exampleDatabase.data
+        .slice()
+        .filter((item: Hero) => {
+          const searchStr = (item.name + item.phone).toLowerCase();
+          return searchStr.indexOf(this.filter.toLowerCase()) != -1;
+        });
+
+      // Sort filtered data
+      const sortedData = this.getSortedData(this.filteredData.slice());
+
+      // Grab the page's slice of data.
+      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+      return sortedData.splice(startIndex, this._paginator.pageSize);
+    });
+  }
+
+  /** Returns a sorted copy of the database data. */
+  getSortedData(data: any[]): any[] {
+    if (!this._sort.active || this._sort.direction === '') {
+      return data;
+    }
+
+    return data.sort((a, b) => {
+      let propertyA: number | string = '';
+      let propertyB: number | string = '';
+
+      switch (this._sort.active) {
+        case 'id':
+          [propertyA, propertyB] = [a.id, b.id];
+          break;
+        case 'name':
+          [propertyA, propertyB] = [a.name, b.name];
+          break;
+      }
+
+      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
+
+      return (
+        (valueA < valueB ? -1 : 1) * (this._sort.direction === 'asc' ? 1 : -1)
+      );
+    });
+  }
+
+  disconnect() {}
 }
